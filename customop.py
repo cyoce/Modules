56681c7dcf5aa81f0d04b5e4af0a431c42c3a7e9
Cyoce <masterryangp@gmail.com>
2015-09-01 15:46:05 -0700

    initial commit

---
+++ customop.py
@@ -0,0 +1,17 @@
+class Infix:
+    def __init__(self, function):
+        self.function = function
+    def __ror__(self, other):
+        return Infix(lambda x, self=self, other=other: self.function(other, x))
+    def __or__(self, other):
+        return self.function(other)
+    def __rlshift__(self, other):
+        return Infix(lambda x, self=self, other=other: self.function(other, x))
+    def __rshift__(self, other):
+        return self.function(other)
+    def __call__(self, value1, value2):
+        return self.function(value1, value2)
+
+#syntax: operator_name = Infix(lambda x,y: <function of x and y>)
+#calling syntax:  x |operator_name| y
+  # or:   x <<operator_name>> y
